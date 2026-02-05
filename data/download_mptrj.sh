408e47f088b21829821256cddc14b68415d9e46c
Teddy Koker <teddy.koker@gmail.com>
2026-02-05 11:21:45 -0800

    update figshare links to use ndownloader.figshare (avoiding 403 for now)

--- data/download_mptrj.sh
+++ data/download_mptrj.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# NOTE: this wget is blocked by figshare, so might need to download manually
+# NOTE: this might be blocked by figshare, so might need to download manually
 # 2023-11-22-mp-trj-extxyz-by-yuan.zip
 if [ ! -f "data/2023-11-22-mp-trj-extxyz-by-yuan.zip" ]; then
-    wget --content-disposition https://figshare.com/files/43302033 -P data/
+    wget --content-disposition https://ndownloader.figshare.com/files/43302033 -P data/
 fi
 
 # unzip data/2023-11-22-mp-trj-extxyz-by-yuan.zip
